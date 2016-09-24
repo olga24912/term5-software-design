009f5217c9d72ba656d33762b4da72c823d5609f
the7winds <vga.grc@gmail.com>
2016-09-24 19:57:08 +0300

    added cd and ls commands

---
+++ cli/src/main/java/ru/spbau/mit/LsCommand.java
@@ -0,0 +1,45 @@
+package ru.spbau.mit;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by the7winds on 24.09.16.
+ */
+
+/**
+ * class for "ls" command: displays all thing inside input directory
+ */
+
+public class LsCommand extends Command {
+
+    @Override
+    public ExecutionResult execute(Environment environment, InputStream stdin) throws IOException {
+        File[] files;
+
+        if (argSize() > 0) {
+            File directory = new File(getArg(0));
+            if (directory.isDirectory()) {
+                files = new File(getArg(0)).listFiles();
+            } else {
+                files = new File[] { directory };
+            }
+        } else {
+            files = new File(System.getProperty("user.dir")).listFiles();
+        }
+
+        StringBuilder output = new StringBuilder();
+
+        for (File file : files) {
+            output.append(file.getName());
+            output.append("\n");
+        }
+
+        ExecutionResult executionResult = new ExecutionResult();
+        executionResult.setStdout(new ByteArrayInputStream(output.toString().getBytes()));
+
+        return executionResult;
+    }
+}
